18aa1a7a180ee929ca0217d6af91710e31bd426b
AryanTitoria <aryantitoria90@gmail.com>
2025-11-02 00:59:02 +0530

    Homepage flow

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
-import { Box, TextField, Button, styled, Typography } from '@mui/material';
-import { API } from '../../service/api';
+import { useState, useContext } from 'react';
 
-// Styled components
+import { Box, TextField, Button, styled, Typography } from '@mui/material';
+
+import { API } from '../../service/api';
+import { DataContext } from '../../context/DataProvider';
+
+import { useNavigate } from 'react-router-dom';
+
 const Component = styled(Box)`
   width: 400px;
   margin: auto;
   box-shadow: 5px 2px 5px 2px rgb(0 0 0 / 0.6);
 `;
+
 
 const Image = styled('img')({
   width: 100,
@@ -15,6 +20,7 @@ const Image = styled('img')({
   display: 'flex',
   padding: '50px 0 0',
 });
+
 
 const Wrapper = styled(Box)`
   padding: 25px 35px;
@@ -28,6 +34,7 @@ const Wrapper = styled(Box)`
   }
 `;
 
+
 const LoginButton = styled(Button)`
   text-transform: none;
   background: #fb641b;
@@ -35,6 +42,7 @@ const LoginButton = styled(Button)`
   height: 48px;
   border-radius: 2px;
 `;
+
 
 const SignupButton = styled(Button)`
   text-transform: none;
@@ -44,6 +52,7 @@ const SignupButton = styled(Button)`
   border-radius: 2px;
   box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
 `;
+
 
 const Text = styled(Typography)`
   color: #878787;
@@ -62,14 +71,16 @@ const signupInitialValues = {
   password: '',
 };
 
-const Login = () => {
-  const imageURL =
-    'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+const Login = ({ isUserAuthenticated }) => {
+  const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
 
   const [account, toggleAccount] = useState('login');
   const [signup, setSignup] = useState(signupInitialValues);
   const [login, setLogin] = useState(loginInitialValues);
   const [error, setError] = useState('');
+
+  const { setAccount } = useContext(DataContext);
+  const navigate = useNavigate();
 
   const toggleSignup = () => {
     setError('');
@@ -96,18 +107,30 @@ const Login = () => {
     }
   };
 
-const onValueChange = (e) => {
-  setLogin({ ...login, [e.target.name]: e.target.value })
-}
-
-const loginUser = async () => {
-  let response = await API.userLogin(login);
-  if(response.isSuccess) {
-    setError('');
-  } else {
-    setError('Something went wrong! Please try again later');
+  const onValueChange = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value })
   }
-}
+
+  const loginUser = async () => {
+    let response = await API.userLogin(login);
+    if(response.isSuccess) {
+      setError('');
+
+      sessionStorage.setItem('accessToken', `Bearer ${response.data.accessToken}`);
+      sessionStorage.setItem('refreshToken', `Bearer ${response.data.refreshToken}`);
+      
+      setAccount({ username: response.data.username, name: response.data.name })
+
+      isUserAuthenticated(true);
+
+      navigate('/');
+
+    } else {
+      setError('Something went wrong! Please try again later');
+
+
+    }
+  }
 
   return (
     <Component>
@@ -116,7 +139,7 @@ const loginUser = async () => {
         {account === 'login' ? (
           <Wrapper>
             <TextField variant="standard" value={login.username} onChange={(e) => onValueChange(e)} name="username" label="Enter Username" />
-            <TextField variant="standard" value={login.password} onChange={(e) => onValueChange(e)} name="username" label="Enter Password" type="password" />
+            <TextField variant="standard" value={login.password} onChange={(e) => onValueChange(e)} name="password" label="Enter Password"  />
 
             { error && <Typography color="error">{error}</Typography> }
             <LoginButton variant="contained" onClick={() => loginUser()} >Login</LoginButton>
--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,6 +1,11 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 import User from '../model/user.js';
+import Token from '../model/token.js';
+
+dotenv.config();
 
 export const signupUser = async (request, response) => {
     try{
@@ -18,21 +23,30 @@ export const signupUser = async (request, response) => {
     }
 }
 
-/*
-
-
-import user from '../model/user.js';
-
-export const signupUser = async (request, response) => {
-    try{
-        const user = request.body;
-
-        const newUser = new UserActivation(user);
-        await newUser.isActive();
-
-        return response.status(200).json({ msg: 'signup successful'})
-    }catch (error){
-        return response.status(500).json({ msg: 'Error while signup the user'})
+export const loginUser = async (request, response) => {
+    let user = await User.findOne({ username: request.body.username });
+    if(!user) {
+        return response.status(400).json({ msg: 'Username does not match'});
     }
-}
-    */
+
+    try {
+        let match = await bcrypt.compare(request.body.password, user.password);
+        if(match){
+            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY , { expiresIn: '15m' });
+            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+
+            const newToken = new Token({ token: refreshToken })
+            await newToken.save();
+
+            return response.status(200).json({accessToken: accessToken, refreshToken: refreshToken, name: user.name, username: user.username })
+
+        } else {
+           return response.status(400).json({ msg: 'Password does not matched' });
+        }
+       // console.log('login request:', request.body);
+       // return response.status(200).json({ msg: 'Login API working fine!' });
+    } catch (error) {
+        console.log('error during login:', error);
+        return response.status(500).json({ msg: 'Error while login', error: error.message });
+    }
+};

3161931ac57fae206626f661453e8b9a92d14cde
AryanTitoria <aryantitoria90@gmail.com>
2025-11-01 13:47:02 +0530

    readded client

---
+++ client/src/constants/config.js
@@ -0,0 +1,31 @@
+//API_NOTIFICATIONS_MESSAGES
+export const API_NOTIFICATIONS_MESSAGES = {
+    loading: {
+        title: 'Loading...',
+        message: 'Data is being loaded, Please wait'
+    },
+    success: {
+        title: 'Success',
+        message: 'Data successfully loaded'
+    },
+    responseFailure: {
+        title: 'Error',
+        message: 'An error occured while fetching response from the server. Please try again'
+    },
+    requestFailure: {
+        title: 'Error',
+        message: 'An error occured while parsing request data'
+    },
+    networkError: {
+        title: 'Error',
+        message: 'unable to connect with the server. Please check the internet connectivity and try again later'
+    }
+}
+
+
+// API service call
+
+
+export const SERVICE_URLS = {
+    userSignup: { url: '/signup', method: 'POST' }
+}

381db285cd0272a2047c36d900c0c7adc973f12e
AryanTitoria <aryantitoria90@gmail.com>
2025-11-01 19:35:57 +0530

    added signup

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,115 +1,146 @@
-import { useState } from 'react'; 
-
+import { useState } from 'react';
 import { Box, TextField, Button, styled, Typography } from '@mui/material';
-
 import { API } from '../../service/api';
 
+// Styled components
 const Component = styled(Box)`
-    width: 400px;
-    margin: auto;
-    box-shadow: 5px2px 5px 2px rgb(0 0 0/ 0.6);
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgb(0 0 0 / 0.6);
 `;
 
 const Image = styled('img')({
-    width: 100,
-    margin: 'auto',
-    display: 'flex',
-    padding: '50px 0 0'
+  width: 100,
+  margin: 'auto',
+  display: 'flex',
+  padding: '50px 0 0',
 });
 
 const Wrapper = styled(Box)`
-    padding: 25px 35px;
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    & > div, & > button, & > p{
-        margin-top: 20px;
-    }
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
 `;
 
 const LoginButton = styled(Button)`
-    text-transform: none;
-    background: #FB641B;
-    color: #fff;
-    height: 48px;
-    border-radius: 2px;
+  text-transform: none;
+  background: #fb641b;
+  color: #fff;
+  height: 48px;
+  border-radius: 2px;
 `;
 
 const SignupButton = styled(Button)`
-    text-transform: none;
-    background: #fff;
-    color: #28740;
-    height: 48px;
-    border-radius: 2px;
-    box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 48px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
 `;
 
 const Text = styled(Typography)`
-    color: #878787;
-    font-size: 16px;
+  color: #878787;
+  font-size: 16px;
 `;
 
-const signupInitialvalues = {
-    name: '',
-    username: '',
-    password: ''
+const loginInitialValues = {
+  username: '',
+  password: ''
 }
 
-const Login =() => {
+// initial values
+const signupInitialValues = {
+  name: '',
+  username: '',
+  password: '',
+};
 
-    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+const Login = () => {
+  const imageURL =
+    'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
 
-    const [account, toggleAccount] = useState('login');
-    const [signup, setSignup] = useState(signupInitialvalues);
-    const [error, seterror] = useState('');
+  const [account, toggleAccount] = useState('login');
+  const [signup, setSignup] = useState(signupInitialValues);
+  const [login, setLogin] = useState(loginInitialValues);
+  const [error, setError] = useState('');
 
-    const toggleSignup = () =>{
-        account === 'signup' ? toggleAccount('login') : toggleAccount('signup');
+  const toggleSignup = () => {
+    setError('');
+    toggleAccount(account === 'signup' ? 'login' : 'signup');
+  };
+
+  const onInputChange = (e) => {
+    setSignup((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const signupUser = async () => {
+    console.log('Signup data being sent:', signup);
+
+    const response = await API.userSignup(signup);
+    if (response.isSuccess) {
+      setError('');
+      setSignup(signupInitialValues);
+      toggleAccount('login');
+    } else {
+      setError('Something went wrong! Please try again later.');
     }
+  };
 
-    const onInputChange = (e) => {
-        setSignup({ ...signup, [e.target.name]: e.target.value });
-    }
-
-    const signupUser = async () => {
-        let response = await API.userSignup(signup);
-        if (response.isSuccess) {
-            seterror('');
-            setSignup(signupInitialvalues);
-            toggleAccount('login')
-        } else {
-            seterror('something went wrong! Please try again later');
-        }
-    }
-
-        return (
-            <Component>
-                <Box>
-                    <img src={imageURL} alt="login" />
-                    {
-                        account === 'login' ?
-                            <Wrapper>
-                                <TextField variant="standard" label="Enter Username"/>
-                                <TextField variant="standard" label="Enter Password"/>
-                                <LoginButton variant="contained"> Login</LoginButton>
-                                <Text style={{ textAlign: 'center'}}>OR</Text>
-                                <SignupButton onClick={() => toggleSignup()}>Create an account</SignupButton>
-                            </Wrapper> 
-                        :
-                            <Wrapper>
-                                <TextField variant="standard" onChange={(e) => onInputChange(e)} name='name' label="Enter Name"></TextField>
-                                <TextField variant="standard" onChange={(e) => onInputChange(e)} name='username' label="Enter Username"/>
-                                <TextField variant="standard" onChange={(e) => onInputChange(e)} name='password' label="Enter Password"/>
-
-                                { error && <Typography>{error}</Typography>}
-                                <SignupButton onClick={() => signupUser()}> Signup</SignupButton>
-                                <Text style={{ textAlign: 'center'}}>OR</Text>
-                                <LoginButton variant="contained" onClick={() => toggleSignup()}>Already have an account</LoginButton>
-                            </Wrapper>
-                    }
-                </Box>
-            </Component>
-        )
+const onValueChange = (e) => {
+  setLogin({ ...login, [e.target.name]: e.target.value })
 }
+
+const loginUser = async () => {
+  let response = await API.userLogin(login);
+  if(response.isSuccess) {
+    setError('');
+  } else {
+    setError('Something went wrong! Please try again later');
+  }
+}
+
+  return (
+    <Component>
+      <Box>
+        <Image src={imageURL} alt="login" />
+        {account === 'login' ? (
+          <Wrapper>
+            <TextField variant="standard" value={login.username} onChange={(e) => onValueChange(e)} name="username" label="Enter Username" />
+            <TextField variant="standard" value={login.password} onChange={(e) => onValueChange(e)} name="username" label="Enter Password" type="password" />
+
+            { error && <Typography color="error">{error}</Typography> }
+            <LoginButton variant="contained" onClick={() => loginUser()} >Login</LoginButton>
+            <Text style={{ textAlign: 'center' }}>OR</Text>
+            <SignupButton onClick={toggleSignup}>Create an account</SignupButton>
+          </Wrapper>
+        ) : (
+          <Wrapper>
+            <TextField variant="standard" onChange={onInputChange} name="name"  label="Enter Name"  value={signup.name}/>
+            <TextField variant="standard" onChange={onInputChange} name="username"  label="Enter Username"  value={signup.username} />
+            <TextField variant="standard" onChange={onInputChange} name="password" label="Enter Password" type="password" value={signup.password } />
+
+            {error && <Typography color="error">{error}</Typography>}
+
+            <SignupButton onClick={signupUser}>Signup</SignupButton>
+            <Text style={{ textAlign: 'center' }}>OR</Text>
+            <LoginButton variant="contained" onClick={toggleSignup}>
+              Already have an account
+            </LoginButton>
+          </Wrapper>
+        )}
+      </Box>
+    </Component>
+  );
+};
 
 export default Login;
--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,3 +1,24 @@
+import bcrypt from 'bcrypt';
+
+import User from '../model/user.js';
+
+export const signupUser = async (request, response) => {
+    try{
+        const hashedPassword = await bcrypt.hash(request.body.password, 10);
+        const userData = { username: request.body.username, name: request.body.name, password: hashedPassword }
+        //const userData = request.body;
+        console.log('signup request: ', userData);
+        const newUser = new User(userData);
+        await newUser.save();
+
+        return response.status(200).json({ msg: 'signup successful'})
+    }catch (error){
+        console.log('error during signup: ', error);
+        return response.status(500).json({ msg: 'Error while signup the user', error: error.message})
+    }
+}
+
+/*
 
 
 import user from '../model/user.js';
@@ -14,3 +35,4 @@ export const signupUser = async (request, response) => {
         return response.status(500).json({ msg: 'Error while signup the user'})
     }
 }
+    */
